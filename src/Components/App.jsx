8eea6840298855436882c6582b84109df750d61c
NadiiaHonchar <17nmg82@gmail.com>
2023-04-06 18:01:27 +0300

    removed Section

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,9 @@
-import Section from "components/Section";
 import { BookTitle } from "components/BookTitle/BookTitle";
 
 export const App = () => {
   return (
     <>
       <BookTitle text="My contacts" />
-      <Section title="First section" />
-      <Section title="Second section">Hello</Section>
     </>
   );
 };
