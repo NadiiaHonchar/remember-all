275d1a80b066f05d5eee97fff4aa390016b4ef54
NadiiaHonchar <17nmg82@gmail.com>
2023-04-07 14:06:08 +0300

    add ContactCard

--- src/Components/ContactBoard/ContactBoard.jsx
+++ src/Components/ContactBoard/ContactBoard.jsx
@@ -1,10 +1,11 @@
+import { ContactCard } from "components/ContactCard/ContactCard";
 import css from "./ContactBoard.module.css";
 
 export const ContactBoard = ({ events }) => {
   return (
     <div className={css.contactBoard}>
-      {events.map((event) => (
-        <div key={event.id}>{event.name}</div>
+      {events.map(({ id, avatar, name, isOnline }) => (
+        <ContactCard key={id} avatar={avatar} name={name} isOnline={isOnline} />
       ))}
     </div>
   );
--- src/Components/ContactCard/ContactCard.jsx
+++ src/Components/ContactCard/ContactCard.jsx
@@ -1,5 +1,21 @@
 import css from "./ContactCard.module.css";
+import { FaTripadvisor } from "react-icons/fa";
 
-export const ContactCard = () => {
-  return <div></div>;
+export const ContactCard = ({ avatar, name, isOnline }) => {
+  console.log("avatar", avatar);
+  console.log("name", name);
+  console.log("isOnline", isOnline);
+  return (
+    <div className={css.item}>
+      <FaTripadvisor className={css.icon} />
+      {isOnline ? (
+        <span className={css.statusTrue}></span>
+      ) : (
+        <span className={css.statusFalse}></span>
+      )}
+
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </div>
+  );
 };
